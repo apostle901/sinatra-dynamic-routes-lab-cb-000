21e1d0f011baa1c99448320aac69c02abd6ec456
Jawann <jawann@gmail.com>
2018-04-09 00:43:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,9 +16,10 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @num = params[:number].to_i
     @phrase = params[:phrase]
+    @return_phrase
     @num.times do
-      "#{@phrase}"
-    end
+      @return_phase << "#{@phrase} "
+    end.@return_phase
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
